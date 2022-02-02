c26d8c5b6b2d754c2b056af53931197e1fed0fed
Arlony Diaz <arlonydiaz@gmail.com>
2022-02-02 12:15:17 -0500

    add darkMode toggle

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import './App.scss';
 import { FaRegMoon, FaMoon } from 'react-icons/fa';
+import { useState } from 'react';
 
 function App() {
+  const [dark_mode, setDark_mode] = useState(false);
   return (
-    <div className='App'>
+    <div className={`${dark_mode ? 'App dark_mode' : 'App'}`}>
       <header className='App-header'>
         <p className='App-header__title'>Where in the world?</p>
-        <div className='dark-mode-toggle'>
-          <FaRegMoon />
+        <div
+          className='dark-mode-toggle'
+          onClick={() => setDark_mode(!dark_mode)}
+        >
+          {dark_mode ? <FaMoon /> : <FaRegMoon />}
           <p className='dark-mode-toggle__text'>Dark Mode</p>
         </div>
       </header>
