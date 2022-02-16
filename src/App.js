d45b78f74e26ca2767e4b7a82577d1f9259ca86e
Arlony Diaz <arlonydiaz@gmail.com>
2022-02-15 20:59:57 -0500

    add Scroll to top on navigation

--- src/App.js
+++ src/App.js
@@ -4,18 +4,21 @@ import Header from './components/Header/Header';
 import CountryInfo from './pages/CountryInfo';
 import Home from './pages/Home';
 import { useGlobalContext } from './context';
+import { ScrollToTop } from './ScrollToTop';
 
 function App() {
   const { dark_mode } = useGlobalContext();
   return (
     <BrowserRouter>
-      <div className={`${dark_mode ? 'App dark_mode' : 'App'}`}>
-        <Header />
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/country/:name' element={<CountryInfo />} />
-        </Routes>
-      </div>
+      <ScrollToTop>
+        <div className={`${dark_mode ? 'App dark_mode' : 'App'}`}>
+          <Header />
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/country/:name' element={<CountryInfo />} />
+          </Routes>
+        </div>
+      </ScrollToTop>
     </BrowserRouter>
   );
 }
